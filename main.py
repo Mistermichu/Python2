d759b044b8e3e81be39947919dc73e7a7c9432aa
MichałM <michalhm@gmail.com>
2023-06-01 21:34:17 +0200

    Item weight user input

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ while not isinstance(package_quantity, int):
         print("Podana wartosc jest niepoprawna, sprobuj ponownie!")
 
 print(f"Ilosc paczek: {package_quantity}")
+
+item_weight = []
+
+for item_weight_input in range(package_quantity):
+    item_weight_input = input("Podaj wartosc paczki x: ")
+    item_weight.append(item_weight_input)
+
+print(item_weight)
