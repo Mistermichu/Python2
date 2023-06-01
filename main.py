b4e4e53b046b701e97ed3564d49dbcb147067249
MichałM <michalhm@gmail.com>
2023-06-01 22:02:34 +0200

    Item weight input loop

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ print(f"Ilosc paczek: {package_quantity}")
 
 item_weight = []
 
-for item_weight_input in range(package_quantity):
-    item_weight_input = input("Podaj wartosc paczki x: ")
-    item_weight.append(item_weight_input)
+for item_weight_input_number in range(package_quantity):
+    
+    item_weight_input = None
+    while not isinstance(item_weight_input, float):
+        try:
+            item_weight_input = input(f"Podaj wage paczki {item_weight_input_number + 1}.: ").replace(",", ".")
+            item_weight_input = float(item_weight_input)
+            item_weight.append(item_weight_input)
+        except ValueError:
+            print("Podana wartosc jest nieporawna, sprobuj ponownie!")
+    
+    
 
 print(item_weight)
