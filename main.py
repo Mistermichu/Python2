ecfd451447cbce975adefb2af6c8428440dbeeef
MichałM <michalhm@gmail.com>
2023-06-01 20:53:24 +0200

    Package quantity

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("Podaj ilość paczek: ")
-package_quantity = input("")
+package_quantity = None
+while not isinstance(package_quantity, int):
+    try:
+        package_quantity = input("Podaj liosc paczek: ")
+        package_quantity = int(package_quantity)
+    except ValueError:
+        print("Podana wartosc jest niepoprawna, sprobuj ponownie!")
+
+print(f"Ilosc paczek: {package_quantity}")
