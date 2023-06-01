ae6923d5531caba9bd3e1a6f8a84d830ed1a444c
MichałM <michalhm@gmail.com>
2023-06-01 22:59:20 +0200

    Item weight range

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-package_quantity = None
-while not isinstance(package_quantity, int):
+print("Witaj w systemie obslugi paczek.") #Done
+print("Pamietaj, ze wysylane przedmioty musza miescic sie w przedziale wagowym od 1kg do 10kg.") #Done
+print("W jednej paczce zmiesci sie 20kg towaru.")
+print("Jezeli dodawany przedmiot przekroczy dozwolona wage paczki, zostanie on wyslany w kolejnej paczce.")
+print("Jezeli dodawany przedmiot bedzie wykraczal poza dozwolony przedzial wagowy, dodawanie przedmiotow zostanie zakonczone.\n") #Done
+
+
+item_quantity = None
+while not isinstance(item_quantity, int):
     try:
-        package_quantity = input("Podaj liosc paczek: ")
-        package_quantity = int(package_quantity)
+        item_quantity = input("Podaj ilosc przedmiotow do wyslania: ")
+        item_quantity = int(item_quantity)
     except ValueError:
         print("Podana wartosc jest niepoprawna, sprobuj ponownie!")
 
-print(f"Ilosc paczek: {package_quantity}")
+print(f"Ilosc przedmiotow: {item_quantity}")
 
 item_weight = []
 
-for item_weight_input_number in range(package_quantity):
+for item_weight_input_number in range(item_quantity):
     
     item_weight_input = None
     while not isinstance(item_weight_input, float):
-        try:
-            item_weight_input = input(f"Podaj wage paczki {item_weight_input_number + 1}.: ").replace(",", ".")
-            item_weight_input = float(item_weight_input)
-            item_weight.append(item_weight_input)
-        except ValueError:
-            print("Podana wartosc jest nieporawna, sprobuj ponownie!")
-    
-    
-
+        while not isinstance(item_weight_input, float):
+            try:
+                item_weight_input = input(f"Podaj wage {item_weight_input_number + 1}. przedmiotu: ").replace(",", ".")
+                item_weight_input = float(item_weight_input)
+            except ValueError:
+                print("Podana wartosc jest nieporawna, sprobuj ponownie!")
+    if item_weight_input < 1 or item_weight_input > 10:
+        break
+    else:
+        item_weight.append(item_weight_input)  
+                
 print(item_weight)
