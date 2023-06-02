6764c321a94a9872dc8705651cb8f8e04d4ccd93
MichałM <michalhm@gmail.com>
2023-06-02 18:06:10 +0200

    Funkcja for WIP

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-print("Witaj w systemie obslugi paczek.") #Done
-print("Pamietaj, ze wysylane przedmioty musza miescic sie w przedziale wagowym od 1kg do 10kg.") #Done
+print("Witaj w systemie obslugi paczek.")  # Done
+print("Pamietaj, ze wysylane przedmioty musza miescic sie w przedziale wagowym od 1kg do 10kg.")  # Done
 print("W jednej paczce zmiesci sie 20kg towaru.")
 print("Jezeli dodawany przedmiot przekroczy dozwolona wage paczki, zostanie on wyslany w kolejnej paczce.")
-print("Jezeli dodawany przedmiot bedzie wykraczal poza dozwolony przedzial wagowy, dodawanie przedmiotow zostanie zakonczone.\n") #Done
+print("Jezeli dodawany przedmiot bedzie wykraczal poza dozwolony przedzial wagowy, dodawanie przedmiotow zostanie zakonczone.\n")  # Done
 
-
+# Wprowadzenie ilości elementów
 item_quantity = None
 while not isinstance(item_quantity, int):
     try:
@@ -15,21 +15,32 @@ while not isinstance(item_quantity, int):
 
 print(f"Ilosc przedmiotow: {item_quantity}")
 
+# Wprowadzenie wagi poszczególnych elementów
 item_weight = []
 
 for item_weight_input_number in range(item_quantity):
-    
+
     item_weight_input = None
     while not isinstance(item_weight_input, float):
         while not isinstance(item_weight_input, float):
             try:
-                item_weight_input = input(f"Podaj wage {item_weight_input_number + 1}. przedmiotu: ").replace(",", ".")
+                item_weight_input = input(
+                    f"Podaj wage {item_weight_input_number + 1}. przedmiotu: ").replace(",", ".")
                 item_weight_input = float(item_weight_input)
             except ValueError:
                 print("Podana wartosc jest nieporawna, sprobuj ponownie!")
     if item_weight_input < 1 or item_weight_input > 10:
+        print("Waga danego przedmiotu nie miesci sie w dozwolonym przedziale wagowym.")
+        print("Dodawanie elementow zostaje zablokowane.")
         break
     else:
-        item_weight.append(item_weight_input)  
-                
+        item_weight.append(item_weight_input)
+
+print("Wyszczegolnione wagi elementow:")
 print(item_weight)
+
+# Obliczenie wagi paczek
+
+for item_number, next_item in enumerate(item_weight):
+    print(f"Element: {item_number}")
+    print(f"Nastepny element: {next_item}")
